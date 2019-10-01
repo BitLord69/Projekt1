ab3cb5a01381262a92a2d75573bd707899958888
BitLord <janis_k@hotmail.com>
2019-10-01 17:02:39 +0200

    Lägg till spårutskrift i Zerba.move Snygga till utskrift av Coordinate och andra-

--- src/com/jcalm/Animal.java
+++ src/com/jcalm/Animal.java
@@ -54,15 +54,18 @@ public abstract class Animal implements CollisionDetector {
         int newCoordinateX = r.nextInt(xMax - xMin) + xMin; // slumpar fram ett x-värde inom rörelsegränsen
         int newCoordinateY = r.nextInt(yMax - yMin) + yMin; // slumpar fram ett y-värde inom rörelsegränsen
 
-
-
         // TODO: 2019-10-01 Blir fel ibland: setX och setY kollar inte gränsvärden, så det kan bli negativa koordinater.
         //  Antingen lägger vi till kontoller i de moetoderna, eller använder moveDelta, som gör kontrollerna redan.
         //  Bäst med moveDelta, eftersom vi då vet varifrån vi kommer och därför kan flippa koorrdinaterna
         coord.setX(newCoordinateX);
         coord.setY(newCoordinateY);
-        System.out.printf("\t\t%s - %s, %s: newX = %d, newY = %d%n", Board.pimpString("Animal.moveRandomly,", Board.LEVEL_NORMAL), Board.pimpString(getClass().getSimpleName(), Board.LEVEL_STRESSED ),this, newCoordinateX, newCoordinateY);
-    }
+        System.out.printf("\t%s%n\t%s - %s, %s: newX = %s, newY = %s%n", "-".repeat(40),
+                Board.pimpString("Animal.moveRandomly,", Board.LEVEL_NORMAL),
+                Board.pimpString(getClass().getSimpleName(), Board.LEVEL_STRESSED),
+                this,
+                Board.pimpString(newCoordinateX, Board.LEVEL_INFO),
+                Board.pimpString(newCoordinateY, Board.LEVEL_INFO));
+    } // moveRandomly
 
     public void move() { // metod för att röra på djuren
         // TODO: 2019-09-29 När Zerba.move är klar, se om man eventuellt kan flytta upp delar av metoderna hit, eller åtminstone lyfta upp delar i andra metoder
